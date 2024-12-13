fb07341c1597d8ffb4e47031448479db4f10d452
D5error <D5error@163.com>
2024-12-13 19:48:30 +0800

    simplify code and add more function

--- src/rm_robot/rm_65_demo/scripts/main.py
+++ src/rm_robot/rm_65_demo/scripts/main.py
@@ -4,58 +4,89 @@ from util import *
 # 长度单位：米
 # 角单位：度
 
-camera_matrix = [   
-    [607.03796387,   0.    ,     326.52359009],
-    [  0.   ,      607.22161865, 244.09521484],
-    [  0.    ,       0.      ,     1.        ]
-]
-
-extrinsic_matrix = [    
-    [-0.51141949,  0.8292543 , -0.22536062, -0.24914425],
-    [ 0.79248142 , 0.35371585, -0.49684836 , 0.28937408],
-    [-0.33230002, -0.43269204 ,-0.83806581 , 0.51372755],
-    [ 0.       ,   0.  ,        0.    ,      1.        ]
-]
+camera_matrix = [[606.9666748 ,   0.  ,       327.58468628],
+ [  0.    ,     605.09527588, 233.15785217],
+ [  0.       ,    0.      ,     1.        ]]
+    
+extrinsic_matrix = [[-0.81605683 , 0.57750925, -0.02311538, -0.17432583],
+ [ 0.52808007,  0.72876161, -0.43593343 , 0.10919362],
+ [-0.23490998, -0.36795322, -0.89968201 , 0.65929079],
+ [ 0.    ,      0.      ,    0.       ,   1.        ]]
 
 hand_parameters = Hand_parameters(
     downward_angle=40,
     hand_length=0.09,
-    z_rotation=0,
+    z_rotation=-20,
     y_offset=-0.015
 )
 
-
-if __name__ == "__main__":
-    rospy.init_node('d5_python_pub', anonymous=True)
+def main():
+    robot = Robot()
 
     # 外参矩阵和内参矩阵
-    # camera_matrix, extrinsic_matrix = get_camera_extrinsic_matrix()
+    camera_matrix, extrinsic_matrix = robot.get_camera_extrinsic_matrix()
 
     # # 声音识别
-    voice_translate(seconds = 3, model="medium")
+    robot.voice_translate(seconds = 3, model="medium")
 
     # 获取物体坐标
-    boundingbox_u, boundingbox_v, depth = get_boundingbox(8)
+    boundingbox_u, boundingbox_v, depth = robot.get_boundingbox(8)
     print(f"boundingbox_u = {boundingbox_u}")
     print(f"boundingbox_u = {boundingbox_v}")
     print(f"boundingbox_u = {depth}")
 
     # 获取物体坐标
-    obj_world_x, obj_world_y, obj_world_z = boundingbox_to_world_coordinate(boundingbox_u, boundingbox_v, depth, camera_matrix, extrinsic_matrix)
+    obj_world_x, obj_world_y, obj_world_z = robot.boundingbox_to_world_coordinate(boundingbox_u, boundingbox_v, depth, camera_matrix, extrinsic_matrix)
     print(f"obj_world_x = {obj_world_x}")
     print(f"obj_world_y = {obj_world_y}")
     print(f"obj_world_z = {obj_world_z}")
 
-
-    move_hand(
+    robot.move_hand(
         hand_x=obj_world_x,
         hand_y=obj_world_y,
-        hand_z=obj_world_z + 0.1,
+        hand_z=obj_world_z + 0.105,
         speed=0.07,
         hand_parameters=hand_parameters
     )
 
-    grip_control(
+    time.sleep(4)
+    
+    robot.grip_control(
         hand_angle=[200, 300, 400, 500, 700, 0],
         hand_speed=100
     )
+
+
+def test():
+    robot = Robot()
+    robot.grip_open()
+    robot.move_hand(
+        hand_x=-0.4,
+        hand_y=-0,
+        hand_z=0.3,
+        speed=0.07,
+        hand_parameters=hand_parameters
+    )
+    
+    robot.move_hand(
+        hand_x=-0.4,
+        hand_y=-0,
+        hand_z=0.11,
+        speed=0.07,
+        hand_parameters=hand_parameters
+    )
+    robot.grip_control(thumb=700, index_finger=700, middle_finger=700, ring_finger=700, little_finger=700)
+
+    robot.move_hand(
+        hand_x=-0.4,
+        hand_y=-0.3,
+        hand_z=0.25,
+        speed=0.07,
+        hand_parameters=hand_parameters
+    )
+
+    robot.grip_open()
+
+if __name__ == "__main__":
+    # main()
+    test()
